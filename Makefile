12e10dd29904be984a4bbc67c442e9c93e998f3a
ScottHAZI <jinghang.scott.hu@gmail.com>
2018-11-30 19:04:36 +0000

    start up code

--- Makefile
+++ Makefile
@@ -1,20 +1,30 @@
-################################################################################
-# Makefile                                                                     #
-#                                                                              #
-# Description: This file contains the make rules for Recitation 1.             #
-#                                                                              #
-# Authors: Athula Balachandran <abalacha@cs.cmu.edu>,                          #
-#          Wolf Richter <wolf@cs.cmu.edu>                                      #
-#                                                                              #
-################################################################################
+CC=gcc
+CFLAGS=-I.
+DEPS = parse.h y.tab.h
+OBJ = y.tab.o lex.yy.o parse.o example.o
+FLAGS = -g -Wall
 
-default: echo_server echo_client
+default:all
+
+all: example echo_server echo_client
+
+lex.yy.c: lexer.l
+	flex $^
+
+y.tab.c: parser.y
+	yacc -d $^
+
+%.o: %.c $(DEPS)
+	$(CC) $(FLAGS) -c -o $@ $< $(CFLAGS)
+
+example: $(OBJ)
+	$(CC) -o $@ $^ $(CFLAGS)
 
 echo_server:
-	@gcc echo_server.c -o echo_server -Wall -Werror
+	$(CC) echo_server.c -o echo_server -Wall -Werror
 
 echo_client:
-	@gcc echo_client.c -o echo_client -Wall -Werror
+	$(CC) echo_client.c -o echo_client -Wall -Werror
 
 clean:
-	@rm echo_server echo_client
+	rm -f *~ *.o example lex.yy.c y.tab.c y.tab.h echo_server echo_client
--- cp1_checker.py
+++ cp1_checker.py
@@ -6,18 +6,17 @@ import random
 import os
 import time
 
-if len(sys.argv) < 7:
+if len(sys.argv) < 6:
     sys.stderr.write('Usage: %s <ip> <port> <#trials>\
             <#writes and reads per trial>\
-            <max # bytes to write at a time> <#connections> \n' % (sys.argv[0]))
+            <#connections> \n' % (sys.argv[0]))
     sys.exit(1)
 
 serverHost = gethostbyname(sys.argv[1])
 serverPort = int(sys.argv[2])
 numTrials = int(sys.argv[3])
 numWritesReads = int(sys.argv[4])
-numBytes = int(sys.argv[5])
-numConnections = int(sys.argv[6])
+numConnections = int(sys.argv[5])
 
 if numConnections < numWritesReads:
     sys.stderr.write('<#connections> should be greater than or equal to <#writes and reads per trial>\n')
@@ -26,8 +25,7 @@ if numConnections < numWritesReads:
 socketList = []
 
 RECV_TOTAL_TIMEOUT = 0.1
-RECV_EACH_TIMEOUT = 0.05
-
+RECV_EACH_TIMEOUT = 0.01
 
 for i in xrange(numConnections):
     s = socket(AF_INET, SOCK_STREAM)
@@ -35,16 +33,30 @@ for i in xrange(numConnections):
     socketList.append(s)
 
 
+GOOD_REQUESTS = ['GET / HTTP/1.1\r\nUser-Agent: 441UserAgent/1.0.0\r\n\r\n']
+BAD_REQUESTS = [
+    'GET\r / HTTP/1.1\r\nUser-Agent: 441UserAgent/1.0.0\r\n\r\n', # Extra CR
+    'GET / HTTP/1.1\nUser-Agent: 441UserAgent/1.0.0\r\n\r\n',     # Missing CR
+    'GET / HTTP/1.1\rUser-Agent: 441UserAgent/1.0.0\r\n\r\n',     # Missing LF
+]
+
+BAD_REQUEST_RESPONSE = 'HTTP/1.1 400 Bad Request\r\n'
+
 for i in xrange(numTrials):
     socketSubset = []
     randomData = []
     randomLen = []
     socketSubset = random.sample(socketList, numConnections)
     for j in xrange(numWritesReads):
-        random_len = random.randrange(1, numBytes)
-        random_string = os.urandom(random_len)
-        randomLen.append(random_len)
-        randomData.append(random_string)
+        random_index = random.randrange(len(GOOD_REQUESTS) + len(BAD_REQUESTS))
+        if random_index < len(GOOD_REQUESTS):
+            random_string = GOOD_REQUESTS[random_index]
+            randomLen.append(len(random_string))
+            randomData.append(random_string)
+        else:
+            random_string = BAD_REQUESTS[random_index - len(GOOD_REQUESTS)]
+            randomLen.append(len(BAD_REQUEST_RESPONSE))
+            randomData.append(BAD_REQUEST_RESPONSE)
         socketSubset[j].send(random_string)
 
     for j in xrange(numWritesReads):
@@ -56,7 +68,6 @@ for i in xrange(numTrials):
             socketSubset[j].settimeout(RECV_EACH_TIMEOUT)
             data += socketSubset[j].recv(randomLen[j])
             if time.time() - start_time > RECV_TOTAL_TIMEOUT:
-		print "Total Time Exceeded"
                 break
         if data != randomData[j]:
             sys.stderr.write("Error: Data received is not the same as sent! \n")
